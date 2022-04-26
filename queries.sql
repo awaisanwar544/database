0fbf8a035f55fb18ba8a726789c8c8b866b10151
awaisanwar544 <awaisanwar544@gmail.com>
2022-04-27 02:56:29 +0500

    Transactions, Agrregate, GROUP by

--- queries.sql
+++ queries.sql
@@ -9,4 +9,27 @@ SELECT * from animals WHERE neutered;
 SELECT * from animals WHERE name != 'Gabumon';
 SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
+/* Transactions */
+UPDATE animals SET species = 'unspecified' RETURNING *;
+ROLLBACK;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon' RETURNING *;
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL RETURNING *;
+COMMIT;
+SELECT * FROM animals;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT del_first;
+UPDATE animals SET weight_kg = weight_kg * -1 RETURNING *;
+ROLLBACK TO del_first;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0 RETURNING *;
+COMMIT;
 
+/* Queries */
+SELECT count(*) FROM animals;
+SELECT count(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, SUM(escape_attempts) as total FROM animals GROUP BY neutered;
+SELECT species, MAX(weight_kg), MIN(weight_kg) FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
